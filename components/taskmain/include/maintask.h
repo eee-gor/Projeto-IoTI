49d65e9fa5117d1a14f25e3c4a2328a0a196024a
eee-gor <84601688+eee-gor@users.noreply.github.com>
2022-11-15 15:52:24 -0300

    Update maintask.h

--- components/taskmain/include/maintask.h
+++ components/taskmain/include/maintask.h
@@ -4,16 +4,20 @@
 #include "esp_timer.h"
 #include "driver/i2c.h"
 #include <math.h>
+#include "esp_sleep.h"
 
 #include "mpu6050.h"
 #include "ds18b20.h"
 #include "ssd1306.h"
 
-#define PAGINA_PIN 2 // botao de mudar pagina está no GPI O2
-#define RESET_PIN 4  // botao de reset está no GPI O4
-#define TEMP_PIN 23  // Sensor de temperatua está no GPIO 23
+#define SLEEP_PIN 5   // botao de mudar pagina está no GPI 18
+#define PAGINA_PIN 18 // botao de mudar pagina está no GPI 18
+#define RESET_PIN 4   // botao de reset está no GPI 19
+#define TEMP_PIN 23   // Sensor de temperatua está no GPIO 23
 
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y)) // função calulo Valor MAXIMO
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y)) // função calulo Valor MINIMO
 
 float calcRMS(float *, int); // Função valor RMS
+
+int abs_val(int x); // Retorna valor absoluto
