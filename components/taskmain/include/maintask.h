0df3f6dced49e41e62811b0802fd1bd7465c451a
eee-gor <84601688+eee-gor@users.noreply.github.com>
2022-11-28 21:26:36 -0300

    Update maintask.h

--- components/taskmain/include/maintask.h
+++ components/taskmain/include/maintask.h
@@ -2,16 +2,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_timer.h"
-#include "driver/i2c.h"
-#include <math.h>
 #include "esp_sleep.h"
+#include "esp_log.h"
 
+#include "driver/i2c.h"
 #include "mpu6050.h"
 #include "ds18b20.h"
 #include "ssd1306.h"
+#include <math.h>
 
 #define RESET_PIN 4   // botao de reset está no GPIO 4
-#define SLEEP_PIN 5   // botao Deep Sleep MODE está no GPIO 5 
+#define SLEEP_PIN 5   // botao Deep Sleep MODE está no GPIO 5
 #define PAGINA_PIN 18 // botao de mudar pagina está no GPIO 18
 #define TEMP_PIN 23   // Sensor de temperatua está no GPIO 23
 
@@ -21,3 +22,5 @@
 float calcRMS(float *, int); // Função valor RMS
 
 int abs_val(int x); // Retorna valor absoluto
+
+static void mqtt_app_start(void);
